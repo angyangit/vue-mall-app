f4b4f1acecd96a3ecf8c3ea70763217255d246c9
angyang <angyangit@163.com>
2021-02-06 21:01:34 +0800

    首页

---
+++ src/request/constant.js
@@ -0,0 +1,5 @@
+const ApiConstant = {
+  //首页内容页信息展示
+  HOME_INFO: '/home/content',
+}
+export default ApiConstant
---
+++ src/request/home.js
@@ -0,0 +1,19 @@
+import axios from './http' // 导入http中创建的axios实例
+import qs from 'qs' // 根据需求是否导入qs模块
+import ApiConstant from './constant'
+
+const HomeAip = {
+  homeInfo() {
+    return axios.get(ApiConstant.HOME_INFO)
+  },
+  articleDetail(id, params) {
+    return axios.get(`${base.sq}/topic/${id}`, {
+      params: params,
+    })
+  },
+  login(params) {
+    return axios.post(`${base.sq}/accesstoken`, qs.stringify(params))
+  },
+}
+
+export default HomeAip
